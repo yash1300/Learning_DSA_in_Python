6621688ab92f5c64385ab79c5cdb997597bbde97
Yash Vishwakarma <84791923+yash1300@users.noreply.github.com>
2024-01-02 01:29:47 +0530

    Add files via upload

---
+++ Leet_DSA/array/11.py
@@ -0,0 +1,20 @@
+# 11. Container With Most Water
+
+# copied for understanding 
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l=0
+        r = len(height)-1
+        height_tracker=0
+        while l<=r:
+            length = r-l
+            h = min(height[l],height[r])
+            if height_tracker<length*h:
+                height_tracker=length*h
+            if height[l]<=height[r]:
+                l+=1
+            else:
+                r-=1
+
+        return height_tracker   
