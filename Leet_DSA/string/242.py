9d2d0d265cbca415e73907f232c00e10fd1820ce
Yash Vishwakarma <84791923+yash1300@users.noreply.github.com>
2024-01-03 15:05:49 +0530

    Add files via upload

---
+++ Leet_DSA/string/242.py
@@ -0,0 +1,9 @@
+# 242 Valid anagram
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False
