979a53a1eec8ac0a88291e893b091b5193130ce3
Yash Vishwakarma <84791923+yash1300@users.noreply.github.com>
2024-01-07 00:40:26 +0530

    Add files via upload

---
+++ Leet_DSA/array/73.py
@@ -0,0 +1,19 @@
+# 73. Set Matrix Zeroes
+
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows = set()
+        cols = set()
+        for row in range(len(matrix)):
+            for col in range(len(matrix[row])):
+                if matrix[row][col] == 0:
+                    rows.add(row)
+                    cols.add(col)
+        for r in range(len(matrix)):
+            for c in range(len(matrix[r])):
+                if r in rows or c in cols:
+                    matrix[r][c] = 0
