4f9b876bef4177f0ef8c2b294a017733500ceddd
Yash Vishwakarma <84791923+yash1300@users.noreply.github.com>
2024-01-10 00:49:57 +0530

    Add files via upload

---
+++ Leet_DSA/string/686.py
@@ -0,0 +1,9 @@
+# 686. Repeated String Match
+
+
+class Solution:
+    def repeatedStringMatch(self, a: str, b: str) -> int:
+        for i in range(len(b)//len(a), len(b)//len(a)+3):
+            if b in a*(i):
+                return i
+        return -1
